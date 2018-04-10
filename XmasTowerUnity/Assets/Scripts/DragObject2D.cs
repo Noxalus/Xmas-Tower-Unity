57653e6ff81de6c1893568c71cae8f8265b2f269
Noxalus <noxalus@gmail.com>
2018-04-10 20:03:04 +0200

    Create a script to drag an object with target joint 2D.

---
+++ XmasTowerUnity/Assets/Scripts/DragObject2D.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject2D : MonoBehaviour
+{
+    public float dampingRatio = 0.5f;
+    public float frequency = 5f;
+
+    private TargetJoint2D targetJoint;
+
+    void Update()
+    {
+        if (!Input.GetMouseButtonDown(0))
+            return;
+
+        RaycastHit2D hit = Physics2D.Raycast(
+             Camera.main.ScreenToWorldPoint(Input.mousePosition),
+            Vector2.zero
+        );
+
+        if (hit.collider == null || !hit.rigidbody || hit.rigidbody.isKinematic)
+            return;
+
+        if (!targetJoint)
+        {
+            targetJoint = gameObject.AddComponent<TargetJoint2D>();
+            targetJoint.dampingRatio = dampingRatio;
+            targetJoint.frequency = frequency;
+        }
+
+        targetJoint.anchor = transform.InverseTransformPoint(hit.point);
+        StartCoroutine(DragObject());
+    }
+
+    IEnumerator DragObject()
+    {
+        while (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetJoint.target = mousePos;
+            yield return null;
+        }
+
+        // Release the input
+        Destroy(targetJoint);
+    }
+}
