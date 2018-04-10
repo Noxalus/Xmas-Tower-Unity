10b904c30f6251f29cffd7e64632c0a504a4213f
Noxalus <noxalus@gmail.com>
2018-04-10 16:36:00 +0200

    Create a script to generate a random, but consistent, gift.

---
+++ XmasTowerUnity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+public class GameManager : MonoBehaviour {
+
+    public bool isGameScreen;
+
+    public SpriteAtlas giftSpriteAtlas;
+    public Gift giftPrefab;
+
+	void Start ()
+    {
+        if (!isGameScreen)
+            AddGift(new Vector2(0, 0));
+    }
+
+    public void AddGift(Vector2 position)
+    {
+        var gift = Instantiate(giftPrefab) as Gift;
+        gift.Initialize(giftSpriteAtlas);
+        gift.transform.position = position;
+    }
+
+	void Update ()
+    {
+	}
+}
---
+++ XmasTowerUnity/Assets/Scripts/Gift.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.U2D;
+
+public class Gift : MonoBehaviour
+{
+    public static readonly string[] BodySpriteType = new string[]
+    {
+        "giftbox_blue_green", "giftbox_blue_pink", "giftbox_blue_red", "giftbox_blue_white", "giftbox_blue_yellow",
+        "giftbox_green_blue", "giftbox_green_pink", "giftbox_green_red", "giftbox_green_white", "giftbox_green_yellow",
+        "giftbox_pink_green", "giftbox_pink_blue", "giftbox_pink_red", "giftbox_pink_white", "giftbox_pink_yellow",
+        "giftbox_red_green", "giftbox_red_blue", "giftbox_red_pink", "giftbox_red_white", "giftbox_red_yellow",
+        "giftbox_white_green", "giftbox_white_pink", "giftbox_white_red", "giftbox_white_blue", "giftbox_white_yellow",
+        "giftbox_yellow_green", "giftbox_yellow_pink", "giftbox_yellow_red", "giftbox_yellow_white", "giftbox_yellow_blue"
+    };
+
+    public static readonly string[] EyeSpriteType = new string[]
+    {
+        "eye1", "eye2", "eye3", "eye4", "eye5", "eye6", "eye7", "eye8"
+    };
+
+    public static readonly string[] MouthSpriteType = new string[]
+    {
+        "mouth1", "mouth2", "mouth3", "mouth4", "mouth5", "mouth6", "mouth7"
+    };
+
+    public static readonly string[] RibbonSpriteType = new string[]
+    {
+        "ribbon1", "ribbon2", "ribbon3", "ribbon4", "ribbon5", "ribbon6"
+    };
+
+    private SpriteRenderer Body;
+    private SpriteRenderer Ribbon;
+    private SpriteRenderer LeftEye;
+    private SpriteRenderer RightEye;
+    private SpriteRenderer Mouth;
+
+    void Awake()
+    {
+        Body = transform.Find("Body").gameObject.GetComponent<SpriteRenderer>();
+        Ribbon = transform.Find("Ribbon").gameObject.GetComponent<SpriteRenderer>();
+        LeftEye = transform.Find("LeftEye").gameObject.GetComponent<SpriteRenderer>();
+        RightEye = transform.Find("RightEye").gameObject.GetComponent<SpriteRenderer>();
+        Mouth = transform.Find("Mouth").gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public void Initialize(SpriteAtlas spriteAtlas)
+    {
+        var defaultEyeSprite = spriteAtlas.GetSprite(EyeSpriteType[0]);
+        var defaultMouthSprite = spriteAtlas.GetSprite(MouthSpriteType[0]);
+        var randomBodySpriteName = BodySpriteType[Random.Range(0, BodySpriteType.Length - 1)];
+
+        Body.sprite = spriteAtlas.GetSprite(randomBodySpriteName);
+        Ribbon.sprite = spriteAtlas.GetSprite(GetCorrespondingRibbon(randomBodySpriteName));
+        LeftEye.sprite = defaultEyeSprite;
+        RightEye.sprite = defaultEyeSprite;
+        Mouth.sprite = defaultMouthSprite;
+    }
+
+    private string GetCorrespondingRibbon(string bodySpriteName)
+    {
+        if (bodySpriteName.EndsWith("_pink"))
+            return "ribbon1";
+        else if (bodySpriteName.EndsWith("_yellow"))
+            return "ribbon2";
+        else if (bodySpriteName.EndsWith("_red"))
+            return "ribbon3";
+        else if (bodySpriteName.EndsWith("_green"))
+            return "ribbon4";
+        else if (bodySpriteName.EndsWith("_white"))
+            return "ribbon5";
+        else if (bodySpriteName.EndsWith("_blue"))
+            return "ribbon6";
+
+        return RibbonSpriteType[0];
+    }
+}
