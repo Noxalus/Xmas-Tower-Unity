c43b6aa69ec0892958f53e4e9c1aae4aa943e955
Noxalus <noxalus@gmail.com>
2018-04-11 16:11:02 +0200

    Add a MusicManager script to play the game music (intro + transition).

---
+++ XmasTowerUnity/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public bool isGameScreen;
+    public AudioClip MenuMusic;
+    public AudioClip GameIntroMusic;
+    public AudioClip GameLoopMusic;
+
+    private AudioSource audioSource;
+    private bool isPlayingIntro;
+
+    void Start () {
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = true;
+
+        if (isGameScreen)
+            StartCoroutine(playGameMusic());
+	}
+
+    private IEnumerator playGameMusic()
+    {
+        audioSource.clip = GameIntroMusic;
+        audioSource.Play();
+        isPlayingIntro = true;
+
+        yield return new WaitForSeconds(audioSource.clip.length);
+
+        audioSource.clip = GameLoopMusic;
+        audioSource.Play();
+        isPlayingIntro = false;
+    }
+
+}
